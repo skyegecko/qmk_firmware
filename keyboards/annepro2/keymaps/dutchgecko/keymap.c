f991cde7a71f00c9e222adabf48c2866946d96d9
Jan-Willem van der Sijp <gecko-git@parseq.co.uk>
2021-02-07 17:29:00 +0100

    Use KC_GESC, update comments with key layout

--- keyboards/annepro2/keymaps/dutchgecko/keymap.c
+++ keyboards/annepro2/keymaps/dutchgecko/keymap.c
@@ -36,7 +36,7 @@ enum anne_pro_layers {
 */
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_BASE_LAYER] = KEYMAP( /* Base */
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
     KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
@@ -51,7 +51,7 @@ enum anne_pro_layers {
   * |-----------------------------------------------------------------------------------------+
   * | Caps    |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
   * |-----------------------------------------------------------------------------------------+
-  * | Shift      |V-UP |V-DWN|MUTE |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
   * |-----------------------------------------------------------------------------------------+
   * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
   * \-----------------------------------------------------------------------------------------/
@@ -69,9 +69,9 @@ enum anne_pro_layers {
   * ,-----------------------------------------------------------------------------------------.
   * |  ~  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON|BRGHT|SPEED| F12 |    Bksp   |
   * |-----------------------------------------------------------------------------------------+
-  * | NumLk    |  7  |  8  |  9  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |BRGT-|BRGT+|    \   |
+  * | Tab      |  7  |  8  |  9  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |BRGT-|BRGT+|   \  |
   * |-----------------------------------------------------------------------------------------+
-  * | Caps    |  4  |  5  |  6  |  f  |  g  |  h  |  j  |  k  | MUT |VOLDN|VOLUP|    Enter    |
+  * | Caps      |  4  |  5  |  6  |  f  |  g  |  h  |  j  |  k  | MUT |VOLDN|VOLUP|   PAUSE   |
   * |-----------------------------------------------------------------------------------------+
   * |      0     |  1  |  2  |  3  |  v  |  b  |  n  |MEDIA|TRKPR|TRKNX|  /  |    Shift       |
   * |-----------------------------------------------------------------------------------------+
