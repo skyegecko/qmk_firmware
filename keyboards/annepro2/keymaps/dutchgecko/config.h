f2cdd86c34345edd8b24c43b5482901c703ce19b
Skye van der Sijp <gecko-git@parseq.co.uk>
2022-11-02 19:21:33 +0100

    Increase polling rate

--- keyboards/annepro2/keymaps/dutchgecko/config.h
+++ keyboards/annepro2/keymaps/dutchgecko/config.h
@@ -1,2 +1,4 @@
 // Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
 #define PERMISSIVE_HOLD
+#define QMK_KEYS_PER_SCAN 4
+#define USB_POLLING_INTERVAL_MS 1
